65431122f7eafe4421335400c84bc6a121ca0f9d
Arthur Beglaryan <arthur.beglaryant@gmail.com>
2025-12-29 19:24:10 +0300

    deploy fix v2

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,16 +1,20 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL!,
-  }).$extends(withAccelerate());
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     accelerateUrl: process.env.DATABASE_URL!,
+//   }).$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL!,
+}).$extends(withAccelerate());
 
 export default prisma;
