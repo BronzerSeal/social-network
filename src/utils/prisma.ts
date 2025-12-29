d6d5eb34e7c526cd9cb0a0d46a528cdc1f1f7cb4
Arthur Beglaryan <arthur.beglaryant@gmail.com>
2025-12-29 19:39:25 +0300

    deploy fix v3

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from "../generated/prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
 
+// v1:
 // const globalForPrisma = global as unknown as {
 //   prisma: PrismaClient;
 // };
@@ -13,8 +15,30 @@ import { withAccelerate } from "@prisma/extension-accelerate";
 
 // if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL!,
-}).$extends(withAccelerate());
+// v2:
+// const prisma = new PrismaClient({
+//   accelerateUrl: process.env.DATABASE_URL!,
+// }).$extends(withAccelerate());
+
+// v3:
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const isProd = process.env.NODE_ENV === "production";
+console.log(isProd);
+
+const prisma = isProd
+  ? new PrismaClient({
+      accelerateUrl: process.env.DATABASE_URL!, // prisma+postgres://
+    }).$extends(withAccelerate())
+  : globalForPrisma.prisma ||
+    new PrismaClient({
+      adapter,
+    });
 
 export default prisma;
