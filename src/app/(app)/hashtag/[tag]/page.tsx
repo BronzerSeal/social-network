10d3e01469deaff4d6c891093e2ce823bc7e67f5
Arthur Beglaryan <arthur.beglaryant@gmail.com>
2026-02-09 22:18:55 +0300

    small bugfix

--- src/app/(app)/hashtag/[tag]/page.tsx
+++ src/app/(app)/hashtag/[tag]/page.tsx
@@ -32,7 +32,7 @@ const HashtagPage = () => {
     readonly unknown[],
     Cursor
   >({
-    queryKey: ["posts", "hashtag-posts"],
+    queryKey: ["posts"],
     queryFn: ({ pageParam }) =>
       getInfinityPostsByHashtag({
         hashtag: `#${tag}`,
